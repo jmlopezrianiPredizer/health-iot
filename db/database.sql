82d3c8716ee818404b5304711b627baa3130cb28
jmlopezrianiJave <jmlopezriani@javerianacali.edu.co>
2025-05-12 14:59:46 -0500

    Cambios multiples

---
+++ db/database.sql
@@ -0,0 +1,11 @@
+CREATE ROLE user WITH LOGIN PASSWORD '12345';
+CREATE DATABASE iotdata OWNER user ENCODING 'UTF8';
+\CONNECT iotdata;
+CREATE TABLE readings(
+    reading_id SERIAL PRIMARY KEY,
+    sensor_id TEXT NOT NULL,
+    heart_rate INTEGER NOT NULL,
+    temperature NUMBER NOT NULL,
+    blood_pressure TEXT NOT NULL,
+    record_time TIMESTAMP NOT NULL DEFAULT now()
+);
